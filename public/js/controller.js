730dae1799e467431f117e6c2431cfb96c824130
ayasemashita <ayase6334@gmail.com>
2023-09-03 22:50:53 +0900

    タッチ操作

--- public/js/controller.js
+++ public/js/controller.js
@@ -50,5 +50,58 @@ export class Controller {
             this.onChange();
             //console.log(event.keyCode);
         });
+
+        //touch
+        const start_pos = {x:0, y:0};
+        let is_moved = false;
+        const canvas = document.getElementById('myCanvas');
+        canvas.addEventListener('touchstart', (event)=>{
+            event.preventDefault();
+            is_moved = false;
+            start_pos.x =  event.touches[0].pageX;
+            start_pos.y =  event.touches[0].pageY;
+        })
+        canvas.addEventListener('touchmove', (event)=>{
+            event.preventDefault();
+            is_moved = true;
+            const pos = {
+                x: event.touches[0].pageX,
+                y: event.touches[0].pageY,
+            };
+            const angle = Math.atan2(pos.y-start_pos.y, pos.x - start_pos.x);
+            const p8 = Math.PI/8;
+            if(angle >= p8 && angle <= p8*7){
+                this.s = true;
+            }else{
+                this.s = false;
+            }
+            if (angle >= -p8*7 && angle <= -p8){
+                this.w = true;
+            }else{
+                this.w = false;
+            }
+            if( angle >= -p8*3 && angle <= p8*3){
+                this.d = true;
+            }else{
+                this.d = false;
+            }
+            if(angle >= p8*5 || angle <= -p8*5){
+                this.a = true;
+            }else { 
+                this.a = false;
+            }
+            this.onChange();
+        });
+        canvas.addEventListener('touchend', (event)=>{
+            event.preventDefault();
+            this.w = this.a = this.s = this.d = false;
+            if(!is_moved){
+                this.shoot = true;
+                this.onChange();
+                this.shoot = false;
+            }
+            this.onChange();
+        });
     }
+
 }
